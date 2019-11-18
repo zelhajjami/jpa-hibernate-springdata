20f2f75e760dff1ca6c11f39f230592e6332c971
zelhajjami <zakariae.el@gmail.com>
2019-11-18 14:55:38 +0100

    Init commit: application.properties configuration - Test table creation

---
+++ src/main/java/org/zelhajjami/JpaHibernateSpringDataApplication.java
@@ -0,0 +1,13 @@
+package org.zelhajjami;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateSpringDataApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/zelhajjami/Product.java
@@ -0,0 +1,55 @@
+package org.zelhajjami;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "product")
+public class Product implements Serializable{
+	@Id
+	@GeneratedValue
+	private Long idProduct;
+	private String appelation;
+	private double price;
+	private int quantity;
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Product(String appelation, double price, int quantity) {
+		super();
+		this.appelation = appelation;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	public Long getIdProduct() {
+		return idProduct;
+	}
+	public void setIdProduct(Long idProduct) {
+		this.idProduct = idProduct;
+	}
+	public String getAppelation() {
+		return appelation;
+	}
+	public void setAppelation(String appelation) {
+		this.appelation = appelation;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	
+}
